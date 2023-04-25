54c428d8ae13da92bf3dcdfc04504a488cf29a08
Reda Chehabi <125459486+RedaChx@users.noreply.github.com>
2023-04-25 21:03:10 +0000

    utils.c
    printf project

---
+++ utils.c
@@ -0,0 +1,86 @@
+#include "main.h"
+
+/**
+ * is_printable - Evaluates if a char is printable
+ * @c: Char to be evaluated.
+ *
+ * Return: 1 if c is printable, 0 otherwise
+ */
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
+}
+
+/**
+ * append_hexadecimal - Append ascci in hexadecimal code to buff
+ * @buff: Array of chars.
+ * @i: Index at which to start appending.
+ * @ascii_code: ASSCI CODE.
+ * Return: Always 3
+ */
+int append_hexadecimal(char ascii_code, char buff[], int i)
+{
+	char map_to[] = "0123456789ABCDEF";
+	/* The hexa format code is always 2 digits long */
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buff[i++] = '\\';
+	buff[i++] = 'x';
+
+	buff[i++] = map_to[ascii_code / 16];
+	buff[i] = map_to[ascii_code % 16];
+
+	return (3);
+}
+
+/**
+ * is_digit - Verifies if a char is a digit
+ * @c: Char to be evaluated
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+
+/**
+ * convert_size_numb - Casts a number to the specified size
+ * @num: Number to be casted.
+ * @size: Number indexicating the type to be casted.
+ *
+ * Return: Casted value of num
+ */
+long int convert_size_numb(long int num, int size)
+{
+	if (size == SIZE_LONG)
+		return (num);
+	else if (size == SIZE_SHORT)
+		return ((short)num);
+
+	return ((int)num);
+}
+
+/**
+ * convrt_size_unsignd - Casts a number to the specified size
+ * @num: Number to be casted
+ * @size: Number indicating the type to be casted
+ *
+ * Return: Casted value of num
+ */
+long int convrt_size_unsignd(unsigned long int num, int size)
+{
+	if (size == SIZE_LONG)
+		return (num);
+	else if (size == SIZE_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
+}
